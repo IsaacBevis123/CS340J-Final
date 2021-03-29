c1f52a1970e7d154445e1531851d93b160c4fc6f
Isaac Bevis <bevisi21@hanover.edu>
2021-03-29 17:05:46 -0600

    Added escape menu

---
+++ Assets/Scripts/EscMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EscMenu : MonoBehaviour
+{
+    private GameObject escMenu;
+    private bool isShowing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        escMenu = GameObject.FindWithTag("EscMenu");
+        escMenu.SetActive(isShowing);
+
+        GameObject escMenu_Disconnect = escMenu.transform.GetChild(2).gameObject;
+        escMenu_Disconnect.GetComponent<Button>().onClick.AddListener(onSelectDisconnect);
+
+        GameObject escMenu_Quit = escMenu.transform.GetChild(3).gameObject;
+        escMenu_Quit.GetComponent<Button>().onClick.AddListener(onSelectQuit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Debug.Log("toggling esc menu");
+            isShowing = !isShowing;
+            escMenu.SetActive(isShowing);
+
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<KartGame.KartSystems.ArcadeKart>().enabled = !isShowing;
+        }
+    }
+
+    void onSelectDisconnect()
+    {
+        Debug.Log("Disconnecting...");
+        // TODO: disconnect from server host here
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    void onSelectQuit()
+    {
+        Debug.Log("Quiting...");
+        Application.Quit();
+    }
+}
