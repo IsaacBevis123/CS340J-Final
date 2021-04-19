89b9d825dee79b57d278450346254db1c1dbee55
Isaac Bevis <bevisi21@hanover.edu>
2021-04-18 22:00:38 -0600

    Fixed Demo track display

--- Assets/Original Assets/Scripts/MainMenu_LoadDemo.cs
+++ Assets/Original Assets/Scripts/MainMenu_LoadDemo.cs
@@ -5,48 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu_LoadDemo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject mainCamera;
-    public UnityEngine.Video.VideoClip playedVideo;
-    private UnityEngine.Video.VideoPlayer videoPlayer;
+    public GameObject Track1Pic;
+    public GameObject Track2Pic;
+    public GameObject Track3Pic;
 
     void Start()
     {
-
-        videoPlayer = mainCamera.AddComponent<UnityEngine.Video.VideoPlayer>();
-
-        // Make sure video doesnt start automatically.
-        videoPlayer.playOnAwake = false;
-
-        videoPlayer.isLooping = true;
-
-        // Make video invisible when not playing
-        videoPlayer.targetCameraAlpha = 0.0f;
+        // make pictures invisible on load
+        Track1Pic.SetActive(false);
+        Track2Pic.SetActive(false);
+        Track3Pic.SetActive(false);
     }
 
     // Executes when cursor passes over the game object
     public void OnPointerEnter(PointerEventData data)
     {
+        // name is local variable that represents the name of the object hovered over
+        //      accesible inside OnPointerEnter provided by unity
+
+        if (name == "Track1")
+        {
+            Track1Pic.SetActive(true);
+        }
+        else if (name == "Track2")
+        {
+            Track2Pic.SetActive(true);
+        }
+        else if (name == "Track3")
+        {
+            Track3Pic.SetActive(true);
+        }
+
         Debug.Log("name: " + name);
-
-        try
-        {
-            videoPlayer.clip = playedVideo;
-            videoPlayer.targetCameraAlpha = 1.0f;
-            videoPlayer.Play();
-        }
-
-        catch (Exception e)
-        {
-            Debug.Log(name + " does not exist.");
-            Debug.Log(e);
-        }
     }
 
     // Executes when cursor leaves the game object
     public void OnPointerExit(PointerEventData data)
     {
-        Debug.Log("leaving...");
-        videoPlayer.targetCameraAlpha = 0.0f;
-        videoPlayer.Stop();
+        Track1Pic.SetActive(false);
+        Track2Pic.SetActive(false);
+        Track3Pic.SetActive(false);
     }
 }
